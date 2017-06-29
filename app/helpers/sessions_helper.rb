f4a39615aef73bc194ca3812c42547337617c5f4
HuyAnh <huyanhhb@yahoo.com.vn>
2017-06-29 12:33:16 +0700

    test

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,7 +5,7 @@ module SessionsHelper
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
-  def log_out(user)
+  def log_out()
     session.delete(:user_id)
     @current_user = nil
   end
