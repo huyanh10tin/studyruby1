0b3dc059659a1138653bdf65c8b40106ebc9826d
HuyAnh <huyanhhb@yahoo.com.vn>
2017-06-29 12:20:43 +0700

    complete session

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 
+  get 'sessions/new'
+
   root "static_pages#home"
   get '/about',to: 'static_pages#about'
   get  '/help',    to: 'static_pages#help', as: 'helf'
@@ -8,6 +10,9 @@ Rails.application.routes.draw do
   get '/home',to: 'static_pages#home'
   get '/signup', to: 'users#new'
   post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users do
     member do
       get :delete
