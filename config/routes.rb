9e9a199f42ac64ca8649c6bd4f9301c0e5e9a69a
HuyAnh <huyanhhb@yahoo.com.vn>
2017-07-12 04:49:25 +0700

    ss

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :users do
-    member do
-      get :delete
-    end
-  end
+  resources :users
   resources :account_activations, only: [:edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
