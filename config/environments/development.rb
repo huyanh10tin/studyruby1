8d8a824982f033312ffb786e84f21573471f0a0f
HuyAnh <huyanhhb@yahoo.com.vn>
2017-07-17 15:00:33 +0700

    test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,12 +27,28 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
-  host = 'localhost:3000' # Don't use this literally; use your local dev host instead
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :test
+  # host = 'localhost:3000'
+  # Don't use this literally; use your local dev host instead
   # config.action_mailer.default_url_options = { host: host, protocol: 'http' }
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Print deprecation notices to the Rails logger.
+  # huyanh
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'secret-fortress-48094.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+  }
+  #end
   config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations.
